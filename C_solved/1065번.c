47271ea9b9dfa9e508527017732c45cf75ae6074
strringcheese <108971444+strringcheese@users.noreply.github.com>
2023-01-31 12:54:26 +0900

    Update 1065번.c

--- C_solved/1065번.c
+++ C_solved/1065번.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    int cnt = 99;
-    scanf("%d", &n);
+    int n; scanf("%d", &n);
+    int cnt = 99
+        
     if (n < 100) printf("%d\n", n);
     else {
-        for (int i=100; i<=n; i++) {
+        for (int i = 100; i <= n; i++) {
             if (i/100-i/10%10 == i/10%10-i%10) cnt++;
         }
         printf("%d\n", cnt);
