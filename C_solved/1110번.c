085c8853acc03b46a48c5a47942f234062ec3684
strringcheese <108971444+strringcheese@users.noreply.github.com>
2023-02-03 17:59:32 +0900

    Update 1110번.c

--- C_solved/1110번.c
+++ C_solved/1110번.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+  int num, cnt=0; scanf("%d",&num);
+  int new_num = num;
+  
+  do {
+    new_num = (new_num % 10) * 10 + ((new_num / 10) + (new_num % 10)) % 10;
+    cnt++;
+  }while(num != new_num);
+
+  printf("%d",cnt);
+  
+  return 0;
+}
